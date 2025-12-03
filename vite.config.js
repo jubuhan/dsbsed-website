bb56be8066c74610f6e81882ed817d34521373ea
jubuhan <jubuhantt5203@gmail.com>
2025-12-03 17:08:17 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Replace 'your-repo-name' with your GitHub repository name
+  base: '/dsbsed-website/', // Replace 'your-repo-name' with your GitHub repository name
 });
